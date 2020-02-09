3f754a9012503bb9282c4d216a4950f21f93504c
Mpho Mphego <mpho112@gmail.com>
2020-02-09 17:01:48 +0200

    typo fix

--- course-02/exercises/udacity-c2-restapi/src/aws.ts
+++ course-02/exercises/udacity-c2-restapi/src/aws.ts
@@ -34,7 +34,7 @@ export function getGetSignedUrl(key: string): string {
 
 /* getPutSignedUrl generates an aws signed url to put an item
  * @Params
- *    key: string - the filename to be retreived from s3 bucket
+ *    key: string - the filename to be retrieved from s3 bucket
  * @Returns:
  *    a url as a string
  */
