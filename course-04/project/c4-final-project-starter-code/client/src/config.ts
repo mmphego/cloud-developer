0a3b52da8c4437c25467f6b276103d12dd42fbbe
Mpho Mphego <mpho112@gmail.com>
2020-04-11 23:33:59 +0200

    Done: Added Auth0 on the app
    Signed-off-by: Mpho Mphego <mpho112@gmail.com>

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -4,7 +4,8 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  // Get from: https://manage.auth0.com/dashboard/us/dev-hhrq1tik/
+  domain: 'dev-hhrq1tik.auth0.com', // Auth0 domain
+  clientId: 'zv2uxvDOZNicGc1gI7dMGTE8Ad30fCEI', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
