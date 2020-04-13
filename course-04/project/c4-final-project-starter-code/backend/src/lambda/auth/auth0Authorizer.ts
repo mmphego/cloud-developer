bec6785d387d703ba6246681a3bb95117886deca
Mpho Mphego <mpho112@gmail.com>
2020-04-13 11:25:05 +0200

    Updated auth0 script pulling cert from json url instead of using client secret evnvar
    Signed-off-by: Mpho Mphego <mpho112@gmail.com>

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -6,23 +6,20 @@ import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
-import { getToken } from '../utils'
+import { getToken } from '../../auth/utils'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = 'https://dev-hhrq1tik.auth0.com/.well-known/jwks.json'
-
-const auth0Secret = process.env.Auth0_Client_Secret
+const jwksUrl = process.env.JWKS_URL
 
 export const handler = async (
   event: CustomAuthorizerEvent
 ): Promise<CustomAuthorizerResult> => {
   logger.info('Authorizing a user', event.authorizationToken)
+
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
+
     logger.info('User was authorized', jwtToken)
 
     return {
@@ -57,12 +54,15 @@ export const handler = async (
   }
 }
 
-async function verifyToken(authHeader: string): Promise<JwtPayload> {
+const verifyToken = async (authHeader: string): Promise<JwtPayload> => {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return verify(token, auth0Secret) as JwtPayload
+  const response = await Axios.get(jwksUrl)
+  const cert_key_id = response.data.keys[0].x5c[0]
+
+  const cert =
+    '-----BEGIN CERTIFICATE-----\n' + cert_key_id + '\n-----END CERTIFICATE-----'
+
+  return verify(token, cert, { algorithms: [jwt.header.alg] }) as JwtPayload
 }
