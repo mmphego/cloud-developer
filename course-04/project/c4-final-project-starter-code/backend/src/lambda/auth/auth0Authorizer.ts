8862e2e6d5254835f99a47340c131c8267a27b78
Mpho Mphego <mpho112@gmail.com>
2020-04-13 06:28:43 +0200

    typo and commented out unused plugins
    Signed-off-by: Mpho Mphego <mpho112@gmail.com>

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -64,5 +64,5 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return verify(token, auth0Secret) as JwtToken
+  return verify(token, auth0Secret) as JwtPayload
 }
